66b6907430fb68c9aea3bac656b81ed204358807
soonsm <soonsm@gmail.com>
2018-03-03 23:49:44 +0900

    temporary agency bot

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const
     body_parser = require('body-parser'),
     app = express().use(body_parser.json()), // creates express http server
     questionBotEventHandler = require('./src/questionBotEventHandler'),
-    agencyBotEventHandler = require('./src/agencyBotEventHandler'),
+    agencyBotEventHandler = require('./src/agencyBot2'),
     httpHandler = require('./src/httpHandler'),
     fbApi = require('./src/fbApi'),
     message = require('./src/message');
@@ -30,7 +30,6 @@ fbApi.sendStart(message.getStart, agencyBotEventHandler.PAGE_ACCESS_TOKEN);
 fbApi.sendGreetingSetting(message.greeting.agency, agencyBotEventHandler.PAGE_ACCESS_TOKEN);
 app.get('/agency_bot/webhook', httpHandler.getHandler(AGENCY_BOT_VERIFY_TOKEN));
 app.post('/agency_bot/webhook', httpHandler.postHandler(agencyBotEventHandler));
-
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
