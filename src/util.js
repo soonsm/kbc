4005b3f7a4f8e736beb3e9f4cc704f2e4485a059
soonsm <soonsm@gmail.com>
2018-03-08 23:41:52 +0900

    날짜 버그 수정

--- src/util.js
+++ src/util.js
@@ -3,7 +3,7 @@ exports.getToday = function(){
     const year = dt.getFullYear();
     const month = dt.getMonth() + 1;
     const day = dt.getDate();
-    return [year, (month < 10 ? '0' + month : month), day].join('');
+    return [year, (month < 10 ? '0' + month : month), (day < 10 ? '0' + day : day)].join('');
 };
 
 
